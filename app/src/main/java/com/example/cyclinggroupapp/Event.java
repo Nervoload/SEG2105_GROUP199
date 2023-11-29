fb02a52b6cf247ff6d0a1355b3d538e80394e3d8
johnbute <john.bute@hotmail.com>
2023-11-28 22:47:56 -0500

    no oopsies

--- app/src/main/java/com/example/cyclinggroupapp/Event.java
+++ app/src/main/java/com/example/cyclinggroupapp/Event.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 //TO CHANGE, NEED TO ADD MORE VARIABLES
 public class Event implements Serializable {
 
-    String EventName, EventRegion, EventType, EventId;
+    String EventName, EventRegion, EventType, EventId, EventOwner;
 
     public String getEventName(){
         return EventName;
@@ -21,4 +21,7 @@ public class Event implements Serializable {
 
     public String getEventId() { return EventId; }
 
+    public String getEventOwner() {
+        return EventOwner;
+    }
 }
